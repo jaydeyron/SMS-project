77289f9a485edd3eb05f19c660bc715868c49947
Jay <94859351+jays56@users.noreply.github.com>
2023-01-09 18:03:46 +0530

    Update AIshit.cpp

--- AIshit.cpp
+++ AIshit.cpp
@@ -91,3 +91,111 @@ class StockSystem {
     } else {
       cout << "Item not found." << endl;
     }
+  // View the current stock record
+  void viewStock() const {
+    cout << "Name\tQuantity\tPrice" << endl;
+    for (int i = 0; i < numItems_; i++) {
+      cout << stock_[i].name() << "\t" << stock_[i].quantity() << "\t\t"
+           << stock_[i].price() << endl;
+    }
+  }
+
+  // Generate a report of the weekly stock activity
+  void generateReport() const {
+    cout << "Weekly stock report:" << endl;
+    cout << "Name\tQuantity\tPrice" << endl;
+    for (int i = 0; i < numItems_; i++) {
+      cout << stock_[i].name() << "\t" << stock_[i].quantity() << "\t\t"
+           << stock_[i].price() << endl;
+    }
+  }
+
+ private:
+  Item stock_[MAX_ITEMS];  // Array to store the stock record
+  int numItems_;  // Number of items currently in the stock record
+};
+
+int main() {
+  StockSystem system;
+
+  // Initialize the stock record with some test data
+  system.addItem(Item("Apples", 50, 0.99));
+  system.addItem(Item("Bananas", 30, 0.79));
+  // ... add more test data here ...
+
+  // Main loop to process user commands
+  while (true) {
+    cout << "Enter command (add/delete/modify/view/report): ";
+    string command;
+    cin >> command;
+
+    if (command == "add") {
+      cout << "Enter name of item to add: ";
+      string name;
+      cin >> name;
+
+      cout << "Enter quantity: ";
+      int quantity;
+      cin >> quantity;
+
+      cout << "Enter price: ";
+      double price;
+      cin >> price;
+
+      system.addItem(Item(name, quantity, price));
+    } else if (command == "delete") {
+      cout << "Enter name of item to delete: ";
+      string name;
+      cin >> name;
+
+      system.deleteItem(name);
+    } else if (command == "modify") {
+      cout << "Enter name of item to modify: ";
+      string name;
+      cin >> name;
+
+      cout << "Enter new quantity: ";
+      int quantity;
+      cin >> quantity;
+
+      cout << "Enter new price: ";
+      double price;
+      cin >> price;
+
+      system.modifyItem(name, quantity, price);
+    } else if (command == "view") {
+      system.viewStock();
+    } else if (command == "report") {
+      system.generateReport();
+    } else {
+      cout << "Invalid command. Try again." << endl;
+    }
+  }
+
+  return 0;
+}
+  // View the current stock record
+  void viewStock() const {
+    cout << "Name\tQuantity\tPrice" << endl;
+    for (int i = 0; i < numItems_; i++) {
+      cout << stock_[i].name() << "\t" << stock_[i].quantity() << "\t\t"
+           << stock_[i].price() << endl;
+    }
+  }
+
+  // Generate a report of the weekly stock activity
+  void generateReport() const {
+    cout << "Weekly stock report:" << endl;
+    cout << "Name\tQuantity\tPrice" << endl;
+    for (int i = 0; i < numItems_; i++) {
+      cout << stock_[i].name() << "\t" << stock_[i].quantity() << "\t\t"
+           << stock_[i].price() << endl;
+    }
+  }
+
+ private:
+  Item stock_[MAX_ITEMS];  // Array to store the stock record
+  int numItems_;  // Number of items currently in the stock record
+};
+
+int main() {
